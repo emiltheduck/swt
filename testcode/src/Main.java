6c5ed841123488b8fe006eb29633fd19b822e4b1
emiltheduck <116971346+emiltheduck@users.noreply.github.com>
2023-06-12 16:47:27 +0200

    Branch

--- testcode/src/Main.java
+++ testcode/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String [] args){
-        System.out.println("Hello World!");
+        printHelloWorld();
         System.out.println("This is a new Main Method!");
+    }
+
+    public static void printHelloWorld(){
+        System.out.println("Hello World!");
     }
 }
