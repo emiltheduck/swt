2a098c4af6c91471ee7eea8fa881ae89838ac408
emiltheduck <116971346+emiltheduck@users.noreply.github.com>
2023-06-12 17:12:01 +0200

    Update Main.java

--- testcode/src/Main.java
+++ testcode/src/Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String [] args){
         printHelloWorld();
         System.out.println("This is a new Main Method!");
+        printName("Emil");
+    }
+
+    public static void printName(String name){
+        System.out.println("My name is " + name);
     }
 
     public static void printHelloWorld(){
