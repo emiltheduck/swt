aafbdb62c3640a13a121b15eb680ec1c7c855636
emiltheduck <116971346+emiltheduck@users.noreply.github.com>
2023-06-12 17:10:38 +0200

    Update Main.java

--- testcode/src/Main.java
+++ testcode/src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
     public static void main(String [] args){
-<<<<<<< HEAD
-=======
         printHelloWorld();
         System.out.println("This is a new Main Method!");
     }
 
     public static void printHelloWorld(){
->>>>>>> parent of efb4a4f (Squashed commit of the following:)
         System.out.println("Hello World!");
     }
 }
